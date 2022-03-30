ebe4284fd62423c9cedd78763d21e462ecedefea
douglas <douglasrg1@gmail.com>
2022-03-30 18:04:29 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Updated
+          Learn React Updated 2
         </a>
       </header>
     </div>
